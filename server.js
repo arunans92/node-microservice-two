e473891ae96403a3614edc0d7634a337a151af21
Arunan <arunan_cs@yahoo.com>
2022-05-16 07:33:15 +0000

    Hello world API Test

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 8001;
+const routes = require('./routes/index');
+const app = express();
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+// To enable CORS => (Cross Origin Resource Sharing)
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Starting points for the routes
+app.use('/api/', routes);
+
+app.listen(port, () => {
+    console.info(`Server started on port ${port}`);
+});
